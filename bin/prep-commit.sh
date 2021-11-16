e118b073fe59a5856915c8c650b21c9e61af7ee7
randompage <steamboatid@gmail.com>
2021-11-16 09:54:30 +0700

    final fixes to phpunit tests, after grunt checks -- tested using phpunit 8.5.21 (PHP 7.3.31) and 9.5.10 (PHP 8.0.12)

---
+++ bin/prep-commit.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# although this script is created to automate repeated processes,
+# but it's recommended to execute each step in this file manually first
+
+dirnow=$(dirname $(realpath $0))
+dirroot=$(realpath "$dirnow/..")
+printf "\n --- ROOT directory: $dirroot \n"
+
+cd $dirroot
+
+#-- delete build directory
+rm -rf $dirroot/build
+
+#-- fresh composer install & update
+rm -rf vendor
+composer install --no-interaction
+composer update --no-interaction --lock
+composer update --no-interaction
+
+#-- install npm-check-updates
+npm install -g npm-check-updates
+ncu -u
+
+#-- fresh install node modules
+npm install -g
+npm install -g grunt-cli
+
+#-- do grunt build
+grunt precommit:verify --force
+grunt precommit
